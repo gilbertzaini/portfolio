efc04a530b3a1753e3fd4627b5e6e81b42604f72
Gilbert Zaini <gilbertzaini03@gmail.com>
2024-06-19 00:44:29 +0700

    fix transition styling

--- src/app/components/transition/transition.jsx
+++ src/app/components/transition/transition.jsx
@@ -1,44 +1,29 @@
 import React, { useRef } from "react";
 import "./transition.css";
 
-export const Transition = (props) => {
+export const Transition = ({ text, inverse }) => {
   const container = useRef(null);
+  // const rotations = [130, 220, 80, 45, 100, 115, 125, 145, 170, 200];
+  const bars = Array.from({ length: 10 }, (_, index) => (
+    <div
+      className="bar"
+      data-scroll
+      data-scroll-speed="0.8"
+      data-scroll-direction="horizontal"
+      key={index}
+      // style={{ transform: `rotate(${rotations[index]}deg)` }}
+    >
+      <p>{text}</p>
+    </div>
+  ));
 
   return (
     <section
       ref={container}
-      className={`transition-container ${props.inverse ? "inverse" : ""}`}
+      className={`transition-container ${inverse ? "inverse" : ""}`}
     >
-      <div className="bar" data-scroll data-scroll-speed="0.7">
-        <p>{props.text}</p>
-      </div>
-      <div className="bar" data-scroll data-scroll-speed="0.7">
-        <p>{props.text}</p>
-      </div>
-      <div className="bar" data-scroll data-scroll-speed="0.7">
-        <p>{props.text}</p>
-      </div>
-      <div className="bar" data-scroll data-scroll-speed="0.7">
-        <p>{props.text}</p>
-      </div>
-      <div className="bar" data-scroll data-scroll-speed="0.7">
-        <p>{props.text}</p>
-      </div>
-      <div className="bar" data-scroll data-scroll-speed="0.7">
-        <p>{props.text}</p>
-      </div>
-      <div className="bar" data-scroll data-scroll-speed="0.7">
-        <p>{props.text}</p>
-      </div>
-      <div className="bar" data-scroll data-scroll-speed="0.7">
-        <p>{props.text}</p>
-      </div>
-      <div className="bar" data-scroll data-scroll-speed="0.7">
-        <p>{props.text}</p>
-      </div>
-      <div className="bar" data-scroll data-scroll-speed="0.7">
-        <p>{props.text}</p>
-      </div>
+      <h1 className="transition-bg">{text}</h1>
+      <div>{bars}</div>
     </section>
   );
 };
