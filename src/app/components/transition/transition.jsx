6817d01a9ff0469dae1c448924575f477a7c5d55
Gilbert Zaini <gilbertzaini03@gmail.com>
2024-06-19 10:34:34 +0700

    ref transition

--- src/app/components/transition/transition.jsx
+++ src/app/components/transition/transition.jsx
@@ -3,7 +3,6 @@ import "./transition.css";
 
 export const Transition = ({ text, inverse }) => {
   const container = useRef(null);
-  const rotations = [0, 20, 40, 60, 80, 90, 110, 130, 150, 170, 190, 210, 230, 250, 180];
 
   return (
     <section
@@ -12,14 +11,12 @@ export const Transition = ({ text, inverse }) => {
     >
       <h1 className="transition-bg">{text}</h1>
       <div>
-        {rotations.map((val, index) => (
+        {Array.from({ length: 15 }).map((_, index) => (
           <div
             key={index}
             className="bar"
             data-scroll
             data-scroll-speed="1.3"
-            data-scroll-direction="horizontal"
-            // style={{ transform: `rotate(${val}deg)` }}
           >
             <p>{text}</p>
           </div>
