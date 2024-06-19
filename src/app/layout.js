8624ededea214c5684cb2824a0dc1a0b539cacaf
Gilbert Zaini <gilbertzaini03@gmail.com>
2024-06-19 09:36:47 +0700

    removed lenis

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import "./lenis.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@
 
 import AOS from "aos";
 import "aos/dist/aos.css";
-import Lenis from "@studio-freight/lenis";
 import { useEffect } from "react";
 import { Hero } from "./components/hero/hero";
 import { Projects } from "./components/projects/projects";
@@ -25,60 +24,10 @@ export default function Home() {
 
   return (
     <div>
-      {/* <section className="secondary-bg" data-scroll-container>
-        <h1 data-scroll data-scroll-direction="vertical">
-          test heading
-        </h1>
-        <h3>Test subheading</h3>
-        <p className="text-heading">test text heading</p>
-        <p className="text-subheading" data-scroll data-scroll-speed="0.6">
-          test text subheading
-        </p>
-        <p>test text</p>
-        <button>test button</button>
-        <div className="secondary-bg" data-scroll data-scroll-speed="1">
-          test
-        </div>
-      </section>
-      <section data-scroll-container>
-        <div>
-          <div
-            className="transition-text mt-5"
-            data-scroll
-            data-scroll-speed="1"
-            data-aos="fade-zoom-in"
-          >
-            EXPERIENCES
-          </div>
-          <div
-            className="transition-text mt-5"
-            data-scroll
-            data-scroll-speed="0.6"
-            data-scroll-direction="horizontal"
-          >
-            EXPERIENCES
-          </div>
-          <div
-            className="transition-text mt-5"
-            data-scroll
-            data-scroll-speed="0.4"
-            data-scroll-direction="horizontal"
-          >
-            EXPERIENCES
-          </div>
-          <div
-            className="transition-text mt-5"
-            data-scroll
-            data-scroll-speed="4"
-          >
-            EXPERIENCES
-          </div>
-        </div>
-      </section> */}
       <Hero />
       <Transition text={"EXPERIENCES"} />
       <Projects />
-      <Transition text={"EXPERIENCES"} inverse={true} />
+      <Transition text={"SKILLS"} inverse={true} />
       <Skills />
     </div>
   );
