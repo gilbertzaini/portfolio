0f6356d8c996d08e000247e8fde54ef04bf6bc09
Gilbert Zaini <gilbertzaini03@gmail.com>
2024-06-22 23:18:13 +0700

    change font to Roboto

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,10 @@
-import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+});
 
 export const metadata = {
   title: "Gilbert Zaini's Portfolio",
@@ -11,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   );
 }
