268771c1eb0c05c129fa579571b9edc5354fe6ff
Gilbert Zaini <gilbertzaini03@gmail.com>
2025-02-05 21:58:55 +0700

    feat: poppins and montserrat font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,30 @@
-import { Roboto } from "next/font/google";
+import { Poppins, Montserrat } from "next/font/google";
 import "./globals.css";
 
-const roboto = Roboto({
+const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "700"],
+  variable: "--font-poppins",
+});
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  variable: "--font-montserrat",
 });
 
 export const metadata = {
   title: "Gilbert Zaini - Portfolio",
-  description: "Explore the portfolio of Gilbert Zaini, a computer engineering student passionate in both hardware and software development. With multiple experiences in IoT development, full-stack web development, and AI model integration, Gilbert has a proven track record in designing hardware components and developing sophisticated web applications. Discover his innovative projects, including MySalak, NemaAPP, and SIKEBAS, and learn about his experience as a Web Developer Intern at PT. Kurnia Ciptamoda Gemilang.",
+  description:
+    "Explore the portfolio of Gilbert Zaini, a computer engineering student passionate in both hardware and software development. With multiple experiences in IoT development, full-stack web development, and AI model integration, Gilbert has a proven track record in designing hardware components and developing sophisticated web applications. Discover his innovative projects, including MySalak, NemaAPP, and SIKEBAS, and learn about his experience as a Web Developer Intern at PT. Kurnia Ciptamoda Gemilang.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={roboto.className}>{children}</body>
+      <body className={`${poppins.variable} ${montserrat.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
